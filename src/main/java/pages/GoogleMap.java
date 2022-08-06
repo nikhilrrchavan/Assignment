df7b7d2ba9975b9d9b375253be325fa9d3c74d77
Admin <Admin@DESKTOP-8651LG6>
2022-08-06 11:31:55 +0530

    second commit

--- src/main/java/pages/GoogleMap.java
+++ src/main/java/pages/GoogleMap.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleMap {
-	//Nikhil
+	//Pooja Nikhil Chavan
 	
 	@FindBy(id="searchboxinput")
 	private WebElement place;
